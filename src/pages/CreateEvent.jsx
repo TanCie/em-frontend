17a3388db0100d639eca5de68871c5b108533edd
Vivek <vg52531@gmail.com>
2025-02-09 01:02:15 +0530

    added text area for event location and a file input for image upload

--- src/pages/CreateEvent.jsx
+++ src/pages/CreateEvent.jsx
@@ -2,20 +2,26 @@ export const CreateEvent = () => {
   return (
     <div className="m-10">
       <div className="flex flex-col gap-10">
-        <div className="text-3xl">
-          Create a new Event
-        </div>
+        <div className="text-3xl">Create a new Event</div>
         <div className="flex justify-between items-center">
           <input type="text" className="input" placeholder="Event Title" />
           <div>Category</div>
         </div>
         <div className="flex justify-between items-center">
-          <input type="textarea" className="input h-24" placeholder="Event Description" />
-          
-          <div>Image</div>
+          <input
+            type="textarea"
+            className="input h-24"
+            placeholder="Event Description"
+          />
+
+          <input
+            type="file"
+            accept="image/*"
+            className="file-input file-input-bordered w-full max-w-xs h-24"
+          />
         </div>
         <div className="flex justify-between items-center">
-          <div>Location</div>
+          <input type="text" className="input" placeholder="Event Location" />
           <div>Date</div>
         </div>
         <button className="btn btn-block">block</button>
