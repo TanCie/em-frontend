6a36f1ea25ff72cd951f07fe5da22c1446a0d531
Tanushree Chakravorty <tanushree998@gmail.com>
2025-02-13 09:50:13 +0530

    404 page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,12 +1,21 @@
 export default function NotFound() {
   return (
-    <div className="text-center mt-30">
+    <div className="bg-gray-800 text-white text-center mt-30">
       <h1>404 - Page Not Found</h1>
       <p>
         The page you are looking for encountered some issue. Please click on the
         link again.
       </p>
-      <a href="/">Go back to Dashboard page</a>
+      <span>
+        Go back to
+        <a
+          className="hover:text-blue-500 hover:underline"
+          href="https://eventrr.vercel.app"
+        >
+          Dashboard
+        </a>{" "}
+        page
+      </span>
     </div>
   );
 }
